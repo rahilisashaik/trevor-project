e791e1d23b0dab0e48466702a2176af00d67c31b
Kevin Guo <kguo28@berkeley.edu>
2025-03-13 08:22:06 -0700

    Most changes made - will make more changes later today

---
+++ Ass3/kev-website/app/contact/page.tsx
@@ -0,0 +1,9 @@
+export default function ContactPage() {
+    return (
+      <div className="p-8">
+        <h1 className="text-3xl font-bold">Contact Me</h1>
+        <p>Email: example@email.com</p>
+        <p>Phone: (123) 456-7890</p>
+      </div>
+    );
+  }
---
+++ Ass3/kev-website/app/resume/page.tsx
@@ -0,0 +1,18 @@
+export default function ResumePage() {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen p-8">
+        <header>
+            Kevin's Experiences
+        </header>
+        <h1 className="text-3xl font-bold mb-4 text-center">
+        <embed
+          src="https://kevinnnnnnnn.tiiny.site"
+          width="800px"
+          height="1000px"
+          className="mx-auto border border-gray-300 shadow-lg"
+        />
+        </h1>
+        
+      </div>
+    );
+}
