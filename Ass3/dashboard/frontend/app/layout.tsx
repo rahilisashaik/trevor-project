91f80591c5457df11e6035e1a35d29a316a60c1f
Emily <emilymo999@berkeley.edu>
2025-03-13 17:19:01 -0700

    [emo] sidebar

--- Ass3/dashboard/frontend/app/layout.tsx
+++ Ass3/dashboard/frontend/app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
+
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,6 +32,15 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+
+        <SidebarProvider>
+          <AppSidebar />
+          <main>
+            <SidebarTrigger />
+            {children}
+          </main>
+        </SidebarProvider>
+
       </body>
     </html>
   );
