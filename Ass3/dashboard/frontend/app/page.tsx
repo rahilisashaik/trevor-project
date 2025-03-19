375a5b096a3ddb7c5977a6ed59c2f07a2a1dca77
Emily <emilymo999@berkeley.edu>
2025-03-19 12:10:08 -0700

    [proj 2a] dashboard

--- Ass3/dashboard/frontend/app/page.tsx
+++ Ass3/dashboard/frontend/app/page.tsx
@@ -1,10 +1,35 @@
 "use client";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 export default function Home() {
   return (
     <div>
       <main>
-        Banana
+      <Card>
+        <CardHeader>
+          <CardTitle><b> PoopyButt </ b></CardTitle>
+          <CardDescription>Patient Info</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p><b>Phone Number: </ b> 696-696-6969</p>
+        </CardContent>
+        <CardContent>
+          <p><b>Time of Last Call: </ b> 07: 2025-07-15</p>
+        </CardContent>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
 
       </main>
       <footer>
